efcc8a90606e8ecb190bb99c8ee21c26ce40d5df
FLOWZ <ketuimevbore@yahoo.com>
2019-11-18 07:17:52 -0800

    implementing a text marquee that makes text slide on screen like on news feed

--- app/src/main/java/com/example/flowz/kotlinapp/MoreActivity.kt
+++ app/src/main/java/com/example/flowz/kotlinapp/MoreActivity.kt
@@ -1,24 +1,34 @@
 package com.example.flowz.kotlinapp
 
-import com.example.flowz.kotlinapp.R
-
-
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 
 import android.graphics.Color
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
+import android.support.v4.app.NotificationCompat
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.PopupMenu
 
+
+
 class MoreActivity : AppCompatActivity() {
+
+    var CHANNEL_ID_ANDROID = "myCHANNEL"
+    var CHANNEL_NAME = "my_Android_Channel"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_more)
 
         val bckgrd = findViewById<ConstraintLayout>(R.id.background)
+
+        val btnnotify = findViewById<Button>(R.id.Notification)
 
         val btn = findViewById<Button>(R.id.d_button)
 
@@ -43,5 +53,37 @@ class MoreActivity : AppCompatActivity() {
             popMenu.show()
 
         }
+
+        btnnotify.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+                val imp = NotificationManager.IMPORTANCE_HIGH
+                val mNotificationChannel = NotificationChannel(CHANNEL_ID_ANDROID, CHANNEL_NAME, imp)
+
+                val notificationBuilder: Notification.Builder = Notification.Builder(this, CHANNEL_ID_ANDROID )
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("Simple Notification...")
+                        .setContentText("This is a simple Notification example!!....")
+                        .setPriority(Notification.PRIORITY_DEFAULT)
+
+                val notificationManager : NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.createNotificationChannel(mNotificationChannel)
+
+                notificationManager.notify(0, notificationBuilder.build())
+            }
+            else
+            {
+                val notificationBuilder2: NotificationCompat.Builder = NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("Simple Notification...")
+                        .setContentText("This is Simple Notification example!!...")
+                val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.notify(0, notificationBuilder2.build())
+
+
+            }
+        }
+
     }
+
 }
--- app/src/main/java/com/example/flowz/kotlinapp/WelcomeActivity.kt
+++ app/src/main/java/com/example/flowz/kotlinapp/WelcomeActivity.kt
@@ -2,10 +2,9 @@ package com.example.flowz.kotlinapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ArrayAdapter
-import android.widget.GridView
-import android.widget.ListView
-import android.widget.Toast
+import android.text.TextUtils
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_more.*
 
 class WelcomeActivity : AppCompatActivity() {
 
@@ -15,7 +14,7 @@ class WelcomeActivity : AppCompatActivity() {
 
         val listView = findViewById<GridView>(R.id.my_list)
 
-        val myArray = arrayOf("CANADA", "AMERICA", "NORWAY", "SPAIN", "GERMANY", "ENGLAND", "SWEEDEN", "ITALY", "FRANCE", "PERU","DENMARK","IRELAND", "ICELAND", "RUSSIA", "FINLAND", "SCOTLAND")
+        val myArray = arrayOf("CANADA", "AMERICA", "NORWAY", "SPAIN", "GERMANY", "ENGLAND", "SWEEDEN", "ITALY", "FRANCE", "PERU", "DENMARK", "IRELAND", "ICELAND", "RUSSIA", "FINLAND", "SCOTLAND")
 
         val myAdapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, myArray)
 
@@ -25,7 +24,18 @@ class WelcomeActivity : AppCompatActivity() {
             Toast.makeText(this, "Item Clicked:- $id", Toast.LENGTH_LONG).show()
         }
 
+        val marquee = findViewById<TextView>(R.id.hello)
+        val button3 = findViewById<Button>(R.id.button)
 
+        button3.setOnClickListener {
+            marquee.text = " Hello world, this is a text Marquee trial, therefore this is a very long sentence on one line, showing what a text marquee does..."
+            marquee.setSingleLine()
+            marquee.ellipsize = TextUtils.TruncateAt.MARQUEE
+            marquee.marqueeRepeatLimit = -1
+            marquee.isSelected = true
+
+
+        }
     }
 
     }
